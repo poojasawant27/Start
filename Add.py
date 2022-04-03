7875cd5e82ef211f2227b1d30f31dbf13844ce57
Pooja <poojasawant2794@gmail.com>
2022-04-03 16:07:02 +0530

    Modified code for addition

--- Add.py
+++ Add.py
@@ -1,5 +1,5 @@
-a=int(input("Enter 1St NUMBER:"))
-b=int(input("Enter 2nd NUMBER:"))
-
-c=a+b
+# Modified Code for Addition
+a = int(input("Enter 1St NUMBER:"))
+b = int(input("Enter 2nd NUMBER:"))
+c = a+b
 print("Addition is:",c)
